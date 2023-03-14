9258092953287b89fe3a3aeb6c8d4882a40502af
ken6078 <ken60786213@gmail.com>
2023-03-14 16:47:53 +0800

    first commit

---
+++ iTunes_Search_Application_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunes_Search_Application_UIKit
+//
+//  Created by Jacky Ben on 2023/3/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
